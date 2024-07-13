526115ddd0e2180211a0d3c2e3fe9be910ad5c8c
mshdabiola <mshdabiola@gmail.com>
2024-07-13 05:31:49 +0100

    feat(topics): display topic category
    This commit displays the topic category in the TopicCard by adding an overlineContent section to the ListItem.
    
    This
     provides users with additional context about the topic.

--- features/topics/src/commonMain/kotlin/com/mshdabiola/topics/TopicsViewModel.kt
+++ features/topics/src/commonMain/kotlin/com/mshdabiola/topics/TopicsViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mshdabiola.data.model.Result
 import com.mshdabiola.data.model.asResult
+import com.mshdabiola.data.repository.ITopicCategory
 import com.mshdabiola.data.repository.ITopicRepository
 import com.mshdabiola.ui.toUi
 import kotlinx.coroutines.flow.SharingStarted
@@ -17,12 +18,13 @@ import kotlinx.coroutines.launch
 
 class TopicsViewModel(
     subjectId: Long,
+    iTopicCategory: ITopicCategory,
     private val topicRepository: ITopicRepository,
 
 ) : ViewModel() {
 
-    val topics = topicRepository
-        .getAllBySubject(subjectId)
+    val topics = iTopicCategory
+        .getTopicBySubject(subjectId)
         .map { topicList -> topicList.map { it.toUi() } }
         .asResult()
         .stateIn(
