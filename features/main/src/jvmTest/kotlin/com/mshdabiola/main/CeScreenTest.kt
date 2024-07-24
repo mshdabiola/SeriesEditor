a705b53c56950feb1688daa2b9314dd771c03b37
mshdabiola <mshdabiola@gmail.com>
2024-07-24 03:26:54 +0100

    test(main): add initial tests for MainViewModel and DetailViewModel
    This commit adds initial tests for `MainViewModel` and `DetailViewModel` in the `
    main` feature module.
    
    Specific changes include:
    
    - Creating test files for `MainViewModelTest`, `DetailViewModelTest`, `CeScreenTest`, `MainViewNoteTest`, and `MainScreenScreenshotTests`.
    - Setting up basic test structures and dependencies.
    - Adding initial test cases for state verification and
     screenshot testing.

---
+++ features/main/src/jvmTest/kotlin/com/mshdabiola/main/CeScreenTest.kt
@@ -0,0 +1,21 @@
+package com.mshdabiola.main
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import kotlin.test.Test
+
+class CeScreenTest {
+
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Test
+    fun main() {
+//        composeRule.setContent {
+//                ComposeExaminationScreen(
+//
+//                )
+//
+//        }
+    }
+}
