5226498c9b7a542ebb8812b3db7ff5cdb5aad46d
mshdabiola <mshdabiola@gmail.com>
2024-07-24 03:25:48 +0100

    test: add screenshot tests
    This commit adds screenshot tests for the Compose Instruction feature.
    
    Specific changes include:
    
    - Adding screenshot tests for the DetailScreen and
     DetailScreenWithText composables.
    - Adding a test for the ForYouScreenPopulatedFeed composable.
    - Configuring the tests to run on Robolectric with native graphics mode.
    - Setting the default time zone to UTC for deterministic tests.

---
+++ features/composeinstruction/src/jvmTest/kotlin/com/mshdabiola/composeinstruction/CeScreenTest.kt
@@ -0,0 +1,21 @@
+package com.mshdabiola.composeinstruction
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import kotlin.test.Test
+
+class CeScreenTest {
+
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Test
+    fun main() {
+//        composeRule.setContent {
+//                ComposeExaminationScreen(
+//
+//                )
+//
+//        }
+    }
+}
