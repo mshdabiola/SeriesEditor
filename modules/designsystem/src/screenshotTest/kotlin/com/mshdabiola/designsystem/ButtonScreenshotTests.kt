d2d41d899e3ed2d3183ecda1e49e8dd038afc2ff
mshdabiola <mshdabiola@gmail.com>
2024-07-24 03:23:29 +0100

    feat(designsystem): add screenshot tests
    This commit adds screenshot tests for various components in the designsystem module, including:
    
    - FilterChip
    - IconButton
    
    - Background
    - Button
    - Tag
    - TopAppBar
    - LoadingWheel
    - Tabs
    
    These tests help ensure the visual consistency and correctness of the components across different themes and font scales.

---
+++ modules/designsystem/src/screenshotTest/kotlin/com/mshdabiola/designsystem/ButtonScreenshotTests.kt
@@ -0,0 +1,37 @@
+/*
+ *abiola 2024
+ */
+
+package com.mshdabiola.designsystem
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mshdabiola.designsystem.component.Capture
+import com.mshdabiola.designsystem.component.CaptureMultiTheme
+import com.mshdabiola.designsystem.component.SeriesEditorButton
+import com.mshdabiola.designsystem.icon.SkIcons
+
+class ButtonScreenshotTests {
+
+    @Preview
+    @Composable
+    fun Button() {
+        CaptureMultiTheme {
+            SeriesEditorButton(onClick = {}, text = { Text(" Button") })
+        }
+    }
+
+    @Preview
+    @Composable
+    fun ButtonWithLeadIcon() {
+        CaptureMultiTheme{
+            SeriesEditorButton(
+                onClick = {},
+                text = { Text("Icon Button") },
+                leadingIcon = { Icon(imageVector = SkIcons.Add, contentDescription = null) },
+            )
+        }
+    }
+}
---
+++ modules/designsystem/src/screenshotTest/kotlin/com/mshdabiola/designsystem/IconButtonScreenshotTests.kt
@@ -0,0 +1,63 @@
+/*
+ *abiola 2023
+ */
+
+package com.mshdabiola.designsystem
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mshdabiola.designsystem.component.CaptureMultiTheme
+import com.mshdabiola.designsystem.component.SkIconToggleButton
+import com.mshdabiola.designsystem.icon.SkIcons
+
+class IconButtonScreenshotTests {
+
+    @Preview
+    @Composable
+    fun ToggleButton() {
+        CaptureMultiTheme{
+            SkIconToggleButton(
+                checked = true,
+                onCheckedChange = { },
+                icon = {
+                    Icon(
+                        imageVector = SkIcons.BookmarkBorder,
+                        contentDescription = null,
+                    )
+                },
+                checkedIcon = {
+                    Icon(
+                        imageVector = SkIcons.Bookmark,
+                        contentDescription = null,
+                    )
+                },
+            )
+        }
+
+    }
+
+    @Preview
+    @Composable
+    fun UnToggleButton() {
+        CaptureMultiTheme{
+            SkIconToggleButton(
+                checked = false,
+                onCheckedChange = { },
+                icon = {
+                    Icon(
+                        imageVector = SkIcons.BookmarkBorder,
+                        contentDescription = null,
+                    )
+                },
+                checkedIcon = {
+                    Icon(
+                        imageVector = SkIcons.Bookmark,
+                        contentDescription = null,
+                    )
+                },
+            )
+        }
+
+    }
+}
