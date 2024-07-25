609192d6df97fc6595957d028853c503432cef56
mshdabiola <mshdabiola@gmail.com>
2024-07-26 00:31:47 +0100

    refactor(composesubject): refactor ComposeSubjectScreen
    This commit refactors the `ComposeSubjectScreen` to improve state management and UI rendering.
    
    Specific changes include:
    
    - Introducing a new sealed class `CsState` to represent the different states of the screen.
    - Using `AnimatedContent` to handle state transitions and provide smooth UI updates.
    - Simplifying the logic for handling series and subject data.
    - Removing unnecessary dependencies and state variables.

--- features/composequestion/src/commonMain/kotlin/com/mshdabiola/composequestion/CqViewModel.kt
+++ features/composequestion/src/commonMain/kotlin/com/mshdabiola/composequestion/CqViewModel.kt
@@ -5,12 +5,8 @@
 package com.mshdabiola.composequestion
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.mshdabiola.data.model.Update
 import com.mshdabiola.data.repository.IExaminationRepository
 import com.mshdabiola.data.repository.IInstructionRepository
 import com.mshdabiola.data.repository.IQuestionRepository
@@ -19,25 +15,20 @@ import com.mshdabiola.data.repository.ITopicCategory
 import com.mshdabiola.generalmodel.QUESTION_TYPE
 import com.mshdabiola.generalmodel.Type
 import com.mshdabiola.model.ImageUtil.getAppPath
-import com.mshdabiola.ui.state.InstructionUiState
 import com.mshdabiola.ui.state.ItemUiState
 import com.mshdabiola.ui.state.OptionUiState
 import com.mshdabiola.ui.state.QuestionUiState
-import com.mshdabiola.ui.state.TopicUiState
 import com.mshdabiola.ui.toInstructionUiState
 import com.mshdabiola.ui.toQuestionUiState
 import com.mshdabiola.ui.toQuestionWithOptions
 import com.mshdabiola.ui.toUi
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -51,99 +42,79 @@ class CqViewModel(
     private val settingRepository: ISettingRepository,
     private val topicCategory: ITopicCategory,
 
-) : ViewModel() {
+    ) : ViewModel() {
 
-    private var _question =
-        mutableStateOf(
-            getEmptyQuestion(),
-        )
-    val question: State<QuestionUiState> = _question
-    private val _update = MutableStateFlow(Update.Edit)
-    val update = _update.asStateFlow()
+//    private var _question =
+//        mutableStateOf(
+//            getEmptyQuestion(),
+//        )
+//    val question: State<QuestionUiState> = _question
+//    private val _update = MutableStateFlow(Update.Edit)
+//    val update = _update.asStateFlow()
 
-    private val _topics = MutableStateFlow(emptyList<TopicUiState>().toImmutableList())
-    val topic = _topics.asStateFlow()
+    private val _cqState = MutableStateFlow<CqState>(CqState.Loading())
+    val cqState = _cqState.asStateFlow()
 
-    val instructs = instructionRepository
-        .getAllByExamId(examId)
-        .map { instructions ->
-            instructions
-                .map {
-                    it.toInstructionUiState()
-                }
-                .toImmutableList()
-        }
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5000),
-            emptyList<InstructionUiState>().toImmutableList(),
-        )
+//    val instructs = instructionRepository
+//        .getAllByExamId(examId)
+//        .map { instructions ->
+//            instructions
+//                .map {
+//                    it.toInstructionUiState()
+//                }
+//                .toImmutableList()
+//        }
+//        .stateIn(
+//            viewModelScope,
+//            SharingStarted.WhileSubscribed(5000),
+//            emptyList<InstructionUiState>().toImmutableList(),
+//        )
 
     var subjectId = -1L
 
     init {
 
         viewModelScope.launch {
+
+            val ques = questionRepository
+                .getOne(questionId)
+                .first()
+
+            _cqState.update {
+                CqState.Success(
+                    questionUiState = ques?.toQuestionUiState(true) ?: getEmptyQuestion(),
+                )
+            }
+
             val examtem = examRepository
                 .getOne(examId)
                 .first()
 
             subjectId = examtem?.subject?.id ?: -1L
 
-            examtem?.subject?.id?.let {
-                topicCategory
-                    .getTopicBySubject(it)
-                    .map { it.map { it.toUi() } }
-                    .collectLatest { topics ->
-                        _topics.update {
-                            topics.toImmutableList()
-                        }
-                    }
-            }
-        }
-        viewModelScope.launch {
-            settingRepository.questions
-                .first()[examId]
-                ?.let {
-                    log(it.toString())
-                    val uiState = it.toQuestionUiState(isEdit = true)
-                    _question.value =
-                        uiState
-                }
+            val topics = topicCategory
+                .getTopicBySubject(subjectId)
+            val instruction = instructionRepository
+                .getAllByExamId(examId)
 
-            val ques = questionRepository
-                .getOne(questionId)
-                .first()
+            combine(topics, instruction) { t, i -> Pair(t, i) }
+                .collectLatest { pair ->
+                    _cqState.update {
+                        if (it is CqState.Success) {
+                            it.copy(
+                                topics = pair.first.map { it.toUi() }.toImmutableList(),
+                                instructs = pair.second.map { it.toInstructionUiState() }
+                                    .toImmutableList(),
+                            )
 
-            if (ques != null) {
-                val uiState = ques.toQuestionUiState(isEdit = true)
-                _question.value =
-                    uiState
-            }
+                        } else
+                            it
 
-            snapshotFlow { question.value }
-                .distinctUntilChanged()
-                .collectLatest {
-
-                    if (it == getEmptyQuestion()) {
-                        val inst = settingRepository.questions
-                            .first()
-                            .toMutableMap()
-                        inst.remove(examId)
-                        settingRepository.setCurrentQuestion(inst)
-                        log("remove")
-                        //  settingRepository.removeQuestion(examId)
-                    } else {
-                        val inst = settingRepository.questions
-                            .first()
-                            .toMutableMap()
-                        inst[examId] = it.toQuestionWithOptions(examId)
-                        settingRepository.setCurrentQuestion(inst)
-                        // settingRepository.setCurrentQuestion(it.toQuestionWithOptions(examId))
                     }
                 }
         }
     }
+
 
     private fun updateExamType(isObjOnly: Boolean) {
         viewModelScope.launch {
@@ -158,7 +129,7 @@ class CqViewModel(
 
     // question edit logic
     fun onAddOption() {
-        var question = _question.value
+        var question = (_cqState.value as CqState.Success).questionUiState
         question = question.copy(
             options = question.options
                 ?.toMutableList()
@@ -175,13 +146,17 @@ class CqViewModel(
                 }
                 ?.toImmutableList(),
 
-        )
+            )
 
-        _question.value = question
+        _cqState.update {
+            if (it is CqState.Success) {
+                it.copy(questionUiState = question)
+            } else it
+        }
     }
 
     fun onAddAnswer(show: Boolean) {
-        var question = _question.value
+        var question = (_cqState.value as CqState.Success).questionUiState
         question = question.copy(
             answers = if (show) {
                 listOf(
@@ -192,11 +167,15 @@ class CqViewModel(
             },
         )
 
-        _question.value = question
+        _cqState.update {
+            if (it is CqState.Success) {
+                it.copy(questionUiState = question)
+            } else it
+        }
     }
 
     fun isTheory(isT: Boolean) {
-        var question = _question.value
+        var question = (_cqState.value as CqState.Success).questionUiState
 
         question = if (isT) {
             question.copy(
@@ -208,10 +187,18 @@ class CqViewModel(
         } else {
             getEmptyQuestion()
         }
-        _question.value = question.copy(isTheory = isT)
+
+        _cqState.update {
+            if (it is CqState.Success) {
+                it.copy(questionUiState = question.copy(isTheory = isT))
+            } else it
+        }
     }
 
-    private fun getEmptyQuestion(optionNo: Int = 4, isTheory: Boolean = false): QuestionUiState {
+    private fun getEmptyQuestion(
+        optionNo: Int = 4,
+        isTheory: Boolean = false,
+    ): QuestionUiState {
         val opNumb = if (isTheory) 0 else optionNo
         return QuestionUiState(
             number = -1,
@@ -311,8 +298,8 @@ class CqViewModel(
             null
         }
         if (removeOption) {
-            val quest = _question.value
-            val options = quest.options?.toMutableList()
+            var question = (_cqState.value as CqState.Success).questionUiState
+            val options = question.options?.toMutableList()
             val option = options?.removeAt(questionIndex)
 
             if (option?.id!! > 0) {
@@ -320,7 +307,13 @@ class CqViewModel(
                     questionRepository.deleteOption(option.id)
                 }
             }
-            _question.value = quest.copy(options = options.toImmutableList())
+
+            _cqState.update {
+                if (it is CqState.Success) {
+                    it.copy(questionUiState = question.copy(options = options.toImmutableList()))
+                } else it
+            }
+
         }
     }
 
@@ -340,11 +333,11 @@ class CqViewModel(
         items: suspend (MutableList<ItemUiState>) -> Int?,
     ) {
         viewModelScope.launch {
-            var quest = _question.value
+            var question = (_cqState.value as CqState.Success).questionUiState
 
             when (questionIndex) {
                 -1 -> {
-                    var qItem = quest.contents.toMutableList()
+                    var qItem = question.contents.toMutableList()
                     val i = items(qItem)
                     if (i != null) {
                         qItem = qItem.mapIndexed { index, itemUi ->
@@ -352,11 +345,11 @@ class CqViewModel(
                         }.toMutableList()
                     }
 
-                    quest = quest.copy(contents = qItem.toImmutableList())
+                    question = question.copy(contents = qItem.toImmutableList())
                 }
 
                 -2 -> {
-                    var qItem = quest.answers?.toMutableList()
+                    var qItem = question.answers?.toMutableList()
                     if (qItem != null) {
                         val i = items(qItem)
                         if (i != null) {
@@ -365,12 +358,12 @@ class CqViewModel(
                             }.toMutableList()
                         }
 
-                        quest = quest.copy(answers = qItem?.toImmutableList())
+                        question = question.copy(answers = qItem?.toImmutableList())
                     }
                 }
 
                 else -> {
-                    val options = quest.options!!.toMutableList()
+                    val options = question.options!!.toMutableList()
                     var option = options[questionIndex]
                     var qItem = option.content.toMutableList()
                     val i = items(qItem)
@@ -383,11 +376,14 @@ class CqViewModel(
                     option = option.copy(content = qItem.toImmutableList())
                     options[questionIndex] = option
 
-                    quest = quest.copy(options = options.toImmutableList())
+                    question = question.copy(options = options.toImmutableList())
                 }
             }
-
-            _question.value = quest
+            _cqState.update {
+                if (it is CqState.Success) {
+                    it.copy(questionUiState = question)
+                } else it
+            }
         }
     }
 
@@ -397,11 +393,12 @@ class CqViewModel(
 
     fun onAddQuestion() {
         viewModelScope.launch {
-            _update.update {
-                Update.Saving
+            var question2 = (_cqState.value as CqState.Success).questionUiState
+
+            _cqState.update {
+                CqState.Loading()
             }
 
-            var question2 = _question.value
             val questions = questionRepository
                 .getByExamId(examId)
                 .first()
@@ -410,21 +407,29 @@ class CqViewModel(
                 if (question2.number == -1L) questions.filter { (it.type == QUESTION_TYPE.ESSAY) == question2.isTheory }.size.toLong() + 1 else question2.number
             question2 = question2.copy(number = number)
 
-            val allIsObj = questions.all { (it.type == QUESTION_TYPE.ESSAY).not() } && question2.isTheory.not()
+            val allIsObj =
+                questions.all { (it.type == QUESTION_TYPE.ESSAY).not() } && question2.isTheory.not()
             questionRepository.upsert(question2.toQuestionWithOptions(examId = examId))
             updateExamType(isObjOnly = allIsObj)
+//
+//                _cqState.update {
+//                    if (it is CqState.Success){
+//                        it.copy(questionUiState = question)
+//                    }
+//                    else it
+//                }
+//
+//                _question.value = getEmptyQuestion(question2.options!!.size, question2.isTheory)
+//
+//// remove from save
+//                val inst = settingRepository.questions
+//                    .first()
+//                    .toMutableMap()
+//                inst.remove(examId)
+//                settingRepository.setCurrentQuestion(inst)
 
-            _question.value = getEmptyQuestion(question2.options!!.size, question2.isTheory)
-
-// remove from save
-            val inst = settingRepository.questions
-                .first()
-                .toMutableMap()
-            inst.remove(examId)
-            settingRepository.setCurrentQuestion(inst)
-
-            _update.update {
-                Update.Success
+            _cqState.update {
+                CqState.Loading(true)
             }
         }
     }
@@ -440,10 +445,32 @@ class CqViewModel(
     }
 
     fun onTopicChange(index: Int) {
-        _question.value = question.value.copy(topicUiState = topic.value.getOrNull(index))
+        var question = (_cqState.value as CqState.Success).questionUiState
+        val topics = (_cqState.value as CqState.Success).topics
+
+        question = question.copy(topicUiState = topics.getOrNull(index))
+
+        _cqState.update {
+            if (it is CqState.Success) {
+                it.copy(questionUiState = question)
+            } else it
+        }
+
+
     }
 
     fun onInstructionChange(index: Int) {
-        _question.value = question.value.copy(instructionUiState = instructs.value.getOrNull(index))
+        var question = (_cqState.value as CqState.Success).questionUiState
+        val instructions = (_cqState.value as CqState.Success).instructs
+
+        question =
+            question.copy(instructionUiState = instructions.getOrNull(index))
+
+        _cqState.update {
+            if (it is CqState.Success) {
+                it.copy(questionUiState = question)
+            } else it
+        }
+
     }
 }
