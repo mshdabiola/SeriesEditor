9b8ae170731661f74836cb8495d6ba71c2d4e524
mshdabiola <mshdabiola@gmail.com>
2024-07-29 03:54:11 +0100

    feat(instructions): add instructions screen
    This commit introduces a new screen for displaying instructions.
    
    Specific changes include:
    
    - Adding a new `InstructionScreen`
     composable to display instructions.
    - Creating an `InstructionsViewModel` to manage the state of the screen.
    - Adding tests for the `InstructionScreen` and `InstructionsViewModel`.
    - Adding screenshot tests for the `InstructionScreen` in both light and dark themes.

---
+++ features/instructions/src/jvmTest/kotlin/com/mshdabiola/instructions/InstructionScreenTest.kt
@@ -0,0 +1,33 @@
+package com.mshdabiola.instructions
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import com.mshdabiola.data.model.Result
+import com.mshdabiola.testing.instructions
+import com.mshdabiola.ui.toInstructionUiState
+import org.junit.Rule
+import kotlin.test.Test
+
+class InstructionScreenTest {
+
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Test
+    fun main() {
+        composeRule.setContent {
+            InstructionScreen(
+                modifier = Modifier.fillMaxSize(),
+                mainState = Result.Success(
+                    instructions.map { it.toInstructionUiState() }
+                )
+            )
+        }
+
+        composeRule.onNodeWithTag("instruction:screen").assertExists()
+        composeRule.onNodeWithTag("instruction:list").assertExists()
+    }
+}
