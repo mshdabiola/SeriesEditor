9723ac8de7e47ebaba83c3fc3e60e7d360eaa92f
mshdabiola <mshdabiola@gmail.com>
2024-08-02 20:19:51 +0100

    feat(designsystem): add export dialog and file access
    This commit adds an export dialog to the designsystem module and implements file access functionality.
    
    Specific changes
     include:
    
    - Introducing a dialog for exporting data with password protection.
    - Implementing platform-specific file path retrieval for desktop and Android.
    - Adding a permission dialog for requesting storage access on Android.
    - Providing a function to check for write permissions.

--- modules/designsystem/src/commonMain/kotlin/com/mshdabiola/designsystem/component/Dialog.kt
+++ modules/designsystem/src/commonMain/kotlin/com/mshdabiola/designsystem/component/Dialog.kt
@@ -1,24 +1,15 @@
 package com.mshdabiola.designsystem.component
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.input.TextFieldLineLimits
-import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.filled.Password
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -26,99 +17,75 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import java.io.File
-import java.io.OutputStream
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainExportDialog(
     show: Boolean,
-    export: (OutputStream, String) -> Unit = { _, _ -> },
+    export: (String, String) -> Unit = { _, _ -> },
     onClose: () -> Unit = {},
 ) {
-    var showDir by remember {
-        mutableStateOf(false)
-    }
-    var path = rememberTextFieldState()
+
+    var path by remember { mutableStateOf<String?>(null) }
 
     val key = rememberTextFieldState()
+    var hasPermission by remember { mutableStateOf(false) }
+
 
     if (show) {
-        Dialog(onDismissRequest = onClose, properties = DialogProperties()) {
-            Card {
-                Column(
-                    modifier = Modifier.padding(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    Text(
-                        modifier = Modifier.align(Alignment.CenterHorizontally),
-                        text = "Export Examination",
-                        style = MaterialTheme.typography.titleMedium,
-                    )
-                    IconButton(onClick = { showDir = true }) {
-                        Icon(
-                            modifier = Modifier.weight(0.1f),
-                            imageVector = Icons.Default.MoreHoriz,
-                            contentDescription = "select",
-                        )
+        HasWrittenPermission {
+            hasPermission = it
+        }
+
+        GetFilePath {
+            path = it?.absolutePath
+
+        }
+
+        if (!hasPermission) {
+            PermissionDialog(
+                onDismiss = onClose,
+                onFile = {
+                    path = it?.absolutePath
+                    if (it != null) {
+                        hasPermission = true
+                    }
+                },
+            )
+        } else {
+            AlertDialog(
+                onDismissRequest = onClose,
+                dismissButton = {
+                    TextButton(onClick = onClose) {
+                        Text("Cancel")
                     }
 
-                    SeriesEditorTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        state = key,
-                        label = "key",
-                        placeholder = "SwordFish",
-                        imeAction = ImeAction.Next,
-                        maxNum = TextFieldLineLimits.SingleLine,
-                    )
-
-                    Row(
-                        modifier = Modifier.align(Alignment.End),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            onClose()
+                            export(path!!, key.text.toString())
+                        },
                     ) {
-                        TextButton(onClick = onClose) {
-                            Text("Cancel")
-                        }
-
-                        Button(
-                            onClick = {
-                                val file = File(path.text.toString(), "data.se")
-                                if (file.parentFile?.exists() == false) {
-                                    file.parentFile?.mkdirs()
-                                }
-                                if (!file.exists()) {
-                                    file.createNewFile()
-                                }
-                                export(
-                                    file.outputStream(),
-                                    key.text.toString().ifBlank { "SwordFish" },
-                                )
-
-                                key.clearText()
-                                onClose()
-                            },
-                            enabled = path.text.isNotBlank(),
-                        ) {
-                            Text("Export")
-                        }
+                        Text("Export")
                     }
-                }
-            }
+                },
+                icon = { Icon(Icons.Default.Password, "password") },
+                title = { Text(text = "Enter password") },
+                text = {
+                    SeriesEditorTextField(
+                        state = key,
+                        label = "Password",
+                    )
+                },
+            )
+
         }
+
     }
-    DirtoryUi(showDir, onDismiss = { showDir = false }) {
-        it?.let {
-            path.edit {
-                this.append(it.path)
-            }
-        }
-    }
+
 }
 
 @Composable
@@ -141,7 +108,7 @@ fun DeleteDialog(
                 ElevatedButton(
                     onClick = onDelete,
 
-                ) {
+                    ) {
                     Text("Delete exam")
                 }
             },
@@ -155,8 +122,16 @@ fun DeleteDialog(
 }
 
 @Composable
-expect fun DirtoryUi(
-    show: Boolean,
+expect fun GetFilePath(
+    onFile: (File?) -> Unit = {},
+)
+
+@Composable
+expect fun HasWrittenPermission(result: (Boolean) -> Unit)
+
+@Composable
+expect fun PermissionDialog(
+    modifier: Modifier = Modifier,
     onDismiss: () -> Unit = {},
     onFile: (File?) -> Unit = {},
 )
--- modules/designsystem/src/main/kotlin/com/mshdabiola/designsystem/component/Dialog.android.kt
+++ modules/designsystem/src/main/kotlin/com/mshdabiola/designsystem/component/Dialog.android.kt
@@ -1,25 +1,158 @@
 package com.mshdabiola.designsystem.component
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Environment
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import java.io.File
 
+
 @Composable
-actual fun DirtoryUi(
-    show: Boolean,
-    onDismiss: () -> Unit,
+actual fun GetFilePath(
     onFile: (File?) -> Unit,
 ) {
     val context = LocalContext.current
+//
+    LaunchedEffect(Unit) {
 
-    LaunchedEffect(key1 = show) {
-        //  if (show){
+        val file = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_DOCUMENTS,
+        ).parentFile
+        val file2 = context.getExternalFilesDir(null)
+        val file3 = ContextCompat.getExternalFilesDirs(context, null)[0]
+        println("file path ${file?.absolutePath}")
+        if (file == null)
+            onFile(null)
+        else
+            onFile(File(file, "series"))
 
-        val file = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
-        onDismiss()
-        onFile(file)
-        // }
     }
+
+
+}
+
+
+@Composable
+actual fun PermissionDialog(
+    modifier: Modifier,
+    onDismiss: () -> Unit,
+    onFile: (File?) -> Unit,
+) {
+    // var showDialog by remember { mutableStateOf(false) }
+    //  var permissionGranted by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+          val file=  Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS,
+            ).parentFile
+            onFile(File(file, "series"))
+        } else {
+            onDismiss()
+        }
+    }
+
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text("File Access Required") },
+        text = { Text("This app needs permission to access files on your device.") },
+        confirmButton = {
+            Button(
+                onClick = {
+                    launcher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                },
+            ) {
+                Text("Grant Permission")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text("Deny")
+            }
+        },
+    )
+
+
+}
+//
+//
+//@Composable
+//fun File(modifier: Modifier = Modifier) {
+//  val ssls = rememberLauncherForActivityResult(
+//      contract = ActivityResultContracts.StartActivityForResult(),
+//  ) {
+//      it
+//  }
+//}
+//
+//@Composable
+//fun RequestStoragePermission() {
+//    var permissionGranted by remember { mutableStateOf(false) }
+//    val context = LocalContext.current
+//    val launcher = rememberLauncherForActivityResult(
+//        contract = ActivityResultContracts.RequestPermission(),
+//    ) { isGranted: Boolean ->
+//        permissionGranted = isGranted
+//    }
+//
+//    if (permissionGranted) {
+//        Text("Storage permission granted!")
+//        // Proceed with file access
+//    } else {
+//        Button(onClick = { launcher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE) }) {
+//            Text("Request storage permission")
+//        }
+//    }
+//}
+//
+//@Composable
+//fun hasWritePermission(): Boolean {
+//    val context = LocalContext.current
+//    return ContextCompat.checkSelfPermission(
+//        context,
+//        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//    ) == PackageManager.PERMISSION_GRANTED
+//}
+//
+//@Composable
+//fun MyComposable() {
+//    if (hasWritePermission()) {
+//        // Write permission is granted, proceed with file writing operations
+//    } else {
+//        // Write permission is not granted, handle accordingly (e.g., request permission)
+//    }
+//}
+//
+//@Composable
+//fun getExternalStorageUriForFile(fileName: String): File? {
+//    val context = LocalContext.current
+//    return context.getExternalFilesDir(null)?.let { directory ->
+//        File(directory, fileName)
+//    }
+//}
+@Composable
+actual fun HasWrittenPermission(result: (Boolean) -> Unit) {
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        result(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            ) == PackageManager.PERMISSION_GRANTED,
+        )
+    }
+
 }
