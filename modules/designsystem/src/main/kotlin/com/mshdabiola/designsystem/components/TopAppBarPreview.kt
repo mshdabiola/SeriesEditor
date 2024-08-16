d7e14e4971cd929aa1ecd42b9b1c7c67feee7ae5
mshdabiola <mshdabiola@gmail.com>
2024-08-16 23:46:48 +0100

    refactor: Remove unused import
    This commit removes an unused import of `ExperimentalMaterial3Api` from the `TopAppBarPreview.kt` file.

--- modules/designsystem/src/main/kotlin/com/mshdabiola/designsystem/components/TopAppBarPreview.kt
+++ modules/designsystem/src/main/kotlin/com/mshdabiola/designsystem/components/TopAppBarPreview.kt
@@ -2,17 +2,13 @@
  *abiola 2024
  */
 
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.mshdabiola.designsystem.components
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.mshdabiola.designsystem.component.SerMainTopAppBar
 import com.mshdabiola.designsystem.component.SerSubTopAppBar
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview("Top App Bar")
 @Composable
 private fun SkTopAppBarPreview() {
@@ -22,7 +18,6 @@ private fun SkTopAppBarPreview() {
         )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview("Top App Bar")
 @Composable
 private fun DetailTopAppBarPreview() {
