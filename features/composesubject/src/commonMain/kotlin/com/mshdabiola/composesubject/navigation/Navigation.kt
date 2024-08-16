756ae84a60e48a4da3e05d118b80660c785c63c4
mshdabiola <mshdabiola@gmail.com>
2024-08-16 12:55:30 +0100

    feat: Add seriesId to navigation arguments
    This commit adds the `seriesId` to the navigation arguments for the `composeSubjectScreen` route. This allows
     for passing the series ID when navigating to the subject screen, enabling the retrieval of relevant subject information based on both the series and subject IDs.

--- features/composesubject/src/commonMain/kotlin/com/mshdabiola/composesubject/navigation/Navigation.kt
+++ features/composesubject/src/commonMain/kotlin/com/mshdabiola/composesubject/navigation/Navigation.kt
@@ -15,35 +15,44 @@ import com.mshdabiola.composesubject.SubjectRoute
 
 const val CS_ROUTE = "cs_route"
 const val SUBJECT_ID = "subject_id"
-const val FULL_CS_ROUTE = "$CS_ROUTE/{$SUBJECT_ID}"
+const val SERIES_ID = "series_id"
+const val FULL_CS_ROUTE = "$CS_ROUTE/{$SERIES_ID}/{$SUBJECT_ID}"
 
 fun NavController.navigateToComposeSubject(
+    seriesId: Long,
     subjectId: Long,
     navOptions: NavOptions = androidx.navigation.navOptions {
         this.launchSingleTop
     },
-) = navigate("$CS_ROUTE/$subjectId", navOptions)
+) = navigate("$CS_ROUTE/$seriesId/$subjectId", navOptions)
 
 fun NavGraphBuilder.composeSubjectScreen(
     modifier: Modifier = Modifier,
     onShowSnack: suspend (String, String?) -> Boolean,
     onFinish: () -> Unit,
+    defaultSeriesId: Long = -1L,
 ) {
     composable(
-        route = "$CS_ROUTE/{$SUBJECT_ID}",
+        route = FULL_CS_ROUTE,
         arguments = listOf(
             navArgument(SUBJECT_ID) {
                 type = NavType.LongType
                 defaultValue = -1L
             },
+            navArgument(SERIES_ID) {
+                type = NavType.LongType
+                defaultValue = defaultSeriesId
+            },
         ),
     ) {
+        val seriesId = it.arguments?.getLong(SERIES_ID) ?: -1L
         val subjectId = it.arguments?.getLong(SUBJECT_ID) ?: -1L
         SubjectRoute(
             modifier = modifier,
             subjectId = subjectId,
             onShowSnack = onShowSnack,
             onFinish = onFinish,
+            seriesId = seriesId,
         )
     }
 }
