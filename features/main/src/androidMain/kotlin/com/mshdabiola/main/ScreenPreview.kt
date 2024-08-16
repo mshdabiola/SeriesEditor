443bc638f0ccd591f4d6dc500e36e887a893ac89
mshdabiola <mshdabiola@gmail.com>
2024-08-16 11:56:51 +0100

    feat: Add screen preview for MainScreen
    This commit adds a screen preview for the `MainScreen` composable, allowing developers to visualize the UI in different
     device configurations during development.
    
    The preview includes a sample `MainState` with a list of series to showcase the UI elements.

---
+++ features/main/src/androidMain/kotlin/com/mshdabiola/main/ScreenPreview.kt
@@ -0,0 +1,17 @@
+package com.mshdabiola.main
+
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mshdabiola.designsystem.DevicePreviews
+
+@DevicePreviews
+@Composable
+private fun ComposeSubjectPreview() {
+    MainScreen(
+        subjectState = rememberTextFieldState("subject"),
+        mainState = MainState(
+            series = listOf(com.mshdabiola.seriesmodel.Series(1, 6, "ask")),
+        ),
+    )
+}
