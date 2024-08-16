0f0bee581a41ef37a890b4b9f93bc13fcfc9e0ae
mshdabiola <mshdabiola@gmail.com>
2024-08-16 11:57:32 +0100

    refactor: Simplify MainState
    This commit simplifies the `MainState` by removing the Loading and Error states, as they were not being utilized effectively.
    
    The `MainState` now directly holds the list of series, streamlining the state management.

--- features/main/src/commonMain/kotlin/com/mshdabiola/main/MainState.kt
+++ features/main/src/commonMain/kotlin/com/mshdabiola/main/MainState.kt
@@ -2,20 +2,7 @@ package com.mshdabiola.main
 
 import com.mshdabiola.seriesmodel.Series
 
-sealed class MainState {
+data class MainState(
+    val series: List<Series> = emptyList(),
 
-    data class Loading(val isLoading: Boolean = false) : MainState()
-    data class Success(
-        val series: List<Series> = emptyList(),
-    ) : MainState()
-
-    data class Error(val exception: Throwable) : MainState()
-}
-
-fun MainState.getSuccess(value: (MainState.Success) -> MainState.Success): MainState {
-    return if (this is MainState.Success) {
-        value(this)
-    } else {
-        this
-    }
-}
+    )
