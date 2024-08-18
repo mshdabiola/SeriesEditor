decbee08b94f999f00f0e6bc14262a6caa5c1915
mshdabiola <mshdabiola@gmail.com>
2024-08-18 13:51:35 +0100

    test: Add unit tests for MainViewModel
    This commit adds unit tests for the MainViewModel to ensure its functionality is working as expected.
    
    - Tests for initializing
     the ViewModel and loading series data.
    - Tests for deleting a series.
    - Tests for updating a series.
    - Tests for adding a new series.

--- features/main/src/jvmTest/kotlin/com/mshdabiola/main/MainViewModelTest.kt
+++ features/main/src/jvmTest/kotlin/com/mshdabiola/main/MainViewModelTest.kt
@@ -4,18 +4,22 @@
 
 package com.mshdabiola.main
 
-import co.touchlab.kermit.Logger
+import app.cash.turbine.test
 import com.mshdabiola.data.repository.ISeriesRepository
-import com.mshdabiola.data.repository.ISubjectRepository
-import com.mshdabiola.data.repository.UserDataRepository
+import com.mshdabiola.data.repository.IUserRepository
 import com.mshdabiola.testing.dataTestModule
 import com.mshdabiola.testing.util.MainDispatcherRule
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.inject
+import kotlin.test.assertEquals
 
 class MainViewModelTest : KoinTest {
 
@@ -32,7 +36,117 @@ class MainViewModelTest : KoinTest {
         this.modules(dataTestModule)
     }
     private val seriesRepository by inject<ISeriesRepository>()
-    private val userdataRepository by inject<UserDataRepository>()
-    private val subjectRepository by inject<ISubjectRepository>()
-    private val logger = Logger
+    private val userdataRepository by inject<IUserRepository>()
+
+
+    @Test
+    fun init() = runTest(mainDispatcherRule.testDispatcher) {
+        val viewModel = MainViewModel(
+           seriesRepository,
+            userdataRepository
+        )
+
+        viewModel
+            .mainState
+            .test {
+                var state = awaitItem()
+
+                assertEquals(0,state.series.size)
+
+                state=awaitItem()
+
+                assertEquals(10,state.series.size)
+                cancelAndIgnoreRemainingEvents()
+
+
+            }
+    }
+
+    @Test
+    fun delete() = runTest(mainDispatcherRule.testDispatcher) {
+        val viewModel = MainViewModel(
+            seriesRepository,
+            userdataRepository
+        )
+
+        viewModel
+            .mainState
+            .test {
+                var state = awaitItem()
+
+                assertEquals(0,state.series.size)
+
+                awaitItem()
+
+                viewModel.deleteClass(1)
+
+                state=awaitItem()
+
+                assertEquals(9,state.series.size)
+                cancelAndIgnoreRemainingEvents()
+
+
+            }
+
+    }
+
+    @Test
+    fun update() = runTest(mainDispatcherRule.testDispatcher) {
+        val viewModel = MainViewModel(
+            seriesRepository,
+            userdataRepository
+        )
+
+        viewModel
+            .mainState
+            .test {
+                var state = awaitItem()
+
+                assertEquals(0,state.series.size)
+
+                awaitItem()
+                val series= seriesRepository.getOne(1).first()!!
+
+                viewModel.updateClass(series.id)
+                delay(2000)
+                assertEquals(series.name, viewModel.classState.text.toString())
+
+                cancelAndIgnoreRemainingEvents()
+
+
+            }
+
+    }
+
+
+    @Test
+    fun add() = runTest(mainDispatcherRule.testDispatcher) {
+        val viewModel = MainViewModel(
+            seriesRepository,
+            userdataRepository
+        )
+
+        viewModel
+            .mainState
+            .test {
+                var state = awaitItem()
+
+                assertEquals(0,state.series.size)
+
+                awaitItem()
+
+                viewModel.classState.edit {
+                    append("Moshood")
+                }
+                viewModel.addClass()
+
+                state=awaitItem()
+                assertEquals("Moshood", state.series.last().name)
+
+                cancelAndIgnoreRemainingEvents()
+
+
+            }
+
+    }
 }
